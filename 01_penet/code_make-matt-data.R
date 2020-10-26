cd17eeb34b9c00d687bcd7e6a378e83107d4083e
vanichols <vnichols@iastate.edu>
2020-10-26 18:05:12 -0500

    add plot averages csv

--- 01_penet/code_make-matt-data.R
+++ 01_penet/code_make-matt-data.R
@@ -5,6 +5,16 @@
 library(maRsden)
 library(tidyverse)
 
+
+mrs_penetrom %>% 
+  left_join(mrs_plotkey) %>% 
+  select(year, doy, block, plot_id, rep_id, rot_trt, harv_crop, depth_cm, resis_kpa) %>% 
+  group_by(year, doy, block, plot_id, rot_trt, harv_crop, depth_cm) %>% 
+  summarise(resis_kpa = mean(resis_kpa)) %>% 
+  write_csv("01_penet/ml_allplotavgpenet.csv")
+
+    
+
 mrs_penetrom %>% 
   ungroup() %>% 
   select(date, doy) %>% 
