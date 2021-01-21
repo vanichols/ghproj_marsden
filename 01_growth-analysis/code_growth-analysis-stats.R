cc660fea840e2e0fecff39a854c8027edf6b9d94
vanichols <vnichols@iastate.edu>
2021-01-21 13:00:54 -0600

    more growth analysis

--- 01_growth-analysis/code_growth-analysis-stats.R
+++ 01_growth-analysis/code_growth-analysis-stats.R
@@ -250,14 +250,105 @@ ggsave("01_growth-analysis/fig_fe-growth-analysis_rue.png")
 # yield components --------------------------------------------------------
 
 
-mrs_earrows %>% 
-  # group_by(year, plot_id) %>% 
-  # summarise(rows_nu = mean(rows_nu, na.rm = T)) %>% 
-  left_join(mrs_krnl500) %>% 
-  pivot_longer(rows_nu:krnl500_g) %>% 
+
+# mixed model fitting -----------------------------------------------------
+
+
+mdG <- groupedData(mass_gpl ~ doy | plot_id, data = md)
+
+
+#--fit model to each group
+lmodG <- nlsList(mass_gpl ~ SSlogis(doy, Asym, xmid, scal), data = mdG) 
+
+plot(intervals(lmodG))
+
+#--Asym and xmid could have random effects added
+
+fmm1 <- nlme(lmodG, random = pdDiag(Asym + xmid ~ 1))
+
+plot(fmm1)
+plot(fmm1, id = 0.000001) #--fernando likes things to be under 2
+plot(fmm1, id = 0.01) #--eek. 
+
+head(coef(fmm1)) #--notice scal is the same for everything
+intervals(fmm1) #--random effect intervals are reasonable
+
+fxf1 <- fixef(fmm1) #--we need these if we want to add an effect of rotation
+
+#--note this an update, it keeps the random effects, we are just adding a fixed effect
+fmm2 <- update(fmm1, 
+               fixed = list(Asym + xmid + scal ~ rot_trt),
+               start = c(fxf1[1], 0, #--Asym
+                         fxf1[2], 0, #--xmid
+                         fxf1[3], 0)) #--scal
+
+fxf2 <- fixef(fmm2) #--now we have a value for cc and cs
+
+anova(fmm2) #--everything is not 0. not that informative
+intervals(fmm2) #--still well constrained estimates
+plot(fmm2, id = 0.01) #--outliers mean maybe we could model the residual variance better. Don't know how to do that.
+
+#--could try having the variance increase with higher values
+fmm3 <- update(fmm2, weights = varPower())
+plot(fmm3, id = 0.01)
+
+#--compare them ?not working grr
+par(mfrow=c(2,1))
+plot(fmm3, id = 0.01, main="varPower")
+plot(fmm2, id = 0.01, main="no variance modelling")
+
+#--FEM does something I don't quite get. 
+# instead of modelling the variance, try adding two random effect levels?
+
+fmm3a <- update(fmm2, random = list(plot_id = pdDiag(Asym + xmid + scal ~ 1),
+                                    yearF = pdDiag(Asym + xmid + scal ~ 1)),
+                groups = ~ yearF/plot_id)
+
+## Fewer outliers, this is a better model
+plot(fmm3a, id = 0.001)
+ 
+
+## Parameter values and contrast among groups
+emmeans(fmm3a, ~ rot_trt, param = "Asym")
+emmeans(fmm3a, ~ rot_trt, param = "xmid")
+emmeans(fmm3a, ~ rot_trt, param = "scal")
+
+## Contrasts
+contrast(emmeans(fmm3a, ~ rot_trt, param = "Asym"), "pairwise")
+contrast(emmeans(fmm3a, ~ rot_trt, param = "xmid"), "pairwise")
+contrast(emmeans(fmm3a, ~ rot_trt, param = "scal"), "pairwise")
+
+
+# look at preds -----------------------------------------------------------
+
+dat_doy <- 
+  mdG %>% 
+  select(plot_id, yearF, rot_trt) %>% 
+  expand_grid(., doy = seq(130, 300, 5))
+
+dat_doy$mass_gpl_pred <- predict(fmm3a, level = 0, newdata = dat_doy)
+
+dat_doy %>% 
   left_join(mrs_plotkey) %>% 
-  ggplot(aes(rot_trt, value, color = rot_trt)) + 
-  #stat_summary(geom = "point") + 
-  #stat_summary(geom = "errorbar", width = 0.5) +
-  geom_jitter() +
-  facet_grid(name~year, scales = "free")
+  ggplot(aes(x = doy, y = mass_gpl_pred)) + 
+  geom_line(aes(color = rot_trt), size = 3) 
+
+
+fmm2.sim1 <- simulate_nlme(fmm3a, nsim = 100, psim = 1, level = 0)
+mdG$mn.s <- apply(fmm2.sim1, 1, mean)
+mdG$mxn.s <- apply(fmm2.sim1, 1, max)
+mdG$mnn.s <- apply(fmm2.sim1, 1, min)
+
+
+ggplot() + 
+  #  geom_point(data = leachG, aes(x = nrate_kgha, y = leaching_kgha)) + 
+  geom_line(data = mdG, aes(x = doy, 
+                               y = prds, 
+                               color = rot_trt), size = 2) +
+  geom_ribbon(data = mdG, 
+              mapping = aes(x = doy, 
+                            ymin = mxn.s, 
+                            ymax = mnn.s, fill = rot_trt), 
+              alpha = 0.5)
+
+
