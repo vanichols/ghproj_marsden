d6db44dade10b74a39b327c8618b78b2dbba00e0
vanichols <vnichols@iastate.edu>
2021-02-24 15:56:20 -0600

    dead leaves

--- 01_phen/code_exp-phen.R
+++ 01_phen/code_exp-phen.R
@@ -1,4 +1,6 @@
 # started 7/29/2020
+# updated: 2/24/2021
+# goal: are there more green leaves in the 4 yr for longer?
 
 library(tidyverse)
 library(maRsden)
@@ -9,7 +11,21 @@ library(maRsden)
 data("mrs_phen")
 data("mrs_plotkey")
 
-d <- mrs_phen
+d <- 
+  mrs_phen %>% 
+#--fill in some things that can be done using our brains
+  mutate(
+    devleaves_nu = case_when(
+      (year == 2018 & pl_stage == "V4") ~ 4,
+      (year == 2018 & pl_stage == "V6") ~ 6,
+      #(year == 2018 & pl_stage == "V14") ~ 14,
+      TRUE ~ devleaves_nu),
+    grleaves_nu = case_when(
+      (year == 2018 & pl_stage == "V4") ~ devleaves_nu,
+      (year == 2018 & pl_stage == "V6") ~ devleaves_nu,
+      #(year == 2018 & pl_stage == "V14") ~ devleaves_nu,
+      TRUE ~ grleaves_nu)
+  )
 
 d %>% 
   filter(year == 2020) ->a
@@ -17,83 +33,64 @@ d %>%
 
 # look at it --------------------------------------------------------------
 
-
-#--this is stupid
-d %>% 
-  filter(year == 2020) %>% 
-  left_join(mrs_plotkey) %>%
-  mutate(deadleaves_nu = devleaves_nu - grleaves_nu) %>%
-  select(year, date, rot_trt, plot, grleaves_nu, deadleaves_nu) %>% 
-  pivot_longer(grleaves_nu:deadleaves_nu) %>% 
-  ggplot(aes(date, value, group = interaction(plot, rot_trt))) + 
-  geom_col(aes(fill = name), position = "dodge", width = 1) + 
-  facet_wrap(~year, scales = "free") + 
-  scale_alpha_discrete(range = c(0.35, 0.9))
-
-
-
-
-
+#--brown stacked on green. Is this right?
 d %>% 
   left_join(mrs_plotkey) %>%
-  filter(year == 2020) %>% 
-  mutate(deadleaves_nu = devleaves_nu - grleaves_nu,
-         pct_dead = deadleaves_nu/devleaves_nu) %>%
-  group_by(year, date, rot_trt) %>% 
-  summarise(dead = mean(pct_dead, na.rm = T)) %>% 
-  ggplot(aes(date, dead, group = rot_trt)) +
-  geom_col(aes(fill = rot_trt), position = "dodge") + 
-  scale_y_continuous(
-                 labels = scales::percent_format()
-    ) 
-
-d %>% 
-  left_join(mrs_plotkey) %>%
-  filter(year == 2020) %>% 
-  mutate(deadleaves_nu = devleaves_nu - grleaves_nu) %>%
-  ggplot(aes(date, deadleaves_nu, group = rot_trt, color = rot_trt)) +
-  stat_summary()
-
-
-d %>% 
-  left_join(mrs_plotkey) %>%
-  filter(year == 2020) %>% 
-  mutate(deadleaves_nu = devleaves_nu - grleaves_nu) %>%
-  ggplot(aes(date, deadleaves_nu, group = rot_trt, color = rot_trt, fill = rot_trt)) +
-  stat_summary(geom = "bar", position = "dodge")
-
-
-#--green stacked on brown, something is wrong, they should add up to ~20
-d %>% 
-  left_join(mrs_plotkey) %>%
-  filter(year == 2020) %>%
   mutate(dead = devleaves_nu - grleaves_nu) %>% 
-  group_by(date, rot_trt) %>% 
+  group_by(year, date, rot_trt) %>% 
   summarise(
     pot = mean(devleaves_nu, na.rm = T),
     gr = mean(grleaves_nu, na.rm = T),
-            br = mean(dead, na.rm = T)) %>%  
+    br = mean(dead, na.rm = T)) %>%  
   filter(!is.nan(br)) %>% 
   pivot_longer(pot:br) %>%
   filter(name !="pot") %>% 
   ggplot(aes(rot_trt, value, fill = name)) +
-  geom_col(position = "stack", color = "black") + 
-  # scale_fill_manual(values = c("gr" = "green4",
-  #                              "br" = "gold2")) + 
-  facet_grid(.~date)
+  geom_col(position = "stack", color = "black", aes(alpha = rot_trt)) + 
+  labs(y = "Number of leaves",
+       fill = "leaves") +
+  scale_fill_manual(values = c("gr" = "green4",
+                               "br" = "gold4")) +
+  facet_grid(year~date)
 
-d %>% 
-  left_join(mrs_plotkey) %>%
-  filter(year == 2020) %>% 
-  filter(date > "2020-07-28") %>% 
-  ggplot(aes(block, devleaves_nu, color = rot_trt)) + 
-  geom_jitter() + 
-  stat_summary(size = 3)
+ggsave("01_phen/fig_deadleaves.png")
 
-d %>% 
+#--start simpler 
+d_simp <- 
+  d %>% 
   left_join(mrs_plotkey) %>%
-  filter(year == 2020) %>% 
-  filter(date > "2020-07-28") %>% 
-  ggplot(aes(rot_trt, devleaves_nu, color = rot_trt)) + 
-  geom_jitter() + 
-  stat_summary(size = 3)
+  filter(!is.na(grleaves_nu)) %>% 
+  select(year, date, doy, rot_trt, block, plot_id, pl_stage, devleaves_nu, grleaves_nu) %>% 
+  mutate(
+    totleaves = devleaves_nu,
+    green = grleaves_nu,
+    dead = totleaves - green)
+
+
+d_simp %>% 
+  group_by(year, doy, rot_trt) %>% 
+  summarise_if(is.numeric, mean, na.rm = T) %>% 
+  ggplot() +
+  geom_point(aes(doy, totleaves), color = "black") +
+  geom_point(aes(doy, green), color = "green") +
+  geom_point(aes(doy, dead), color = "brown") +
+  facet_grid(rot_trt ~year)
+
+
+d_simp %>% 
+  group_by(year, doy, rot_trt) %>% 
+  summarise_if(is.numeric, mean, na.rm = T) %>% 
+  ggplot() +
+  geom_ribbon(aes(doy, ymin = 0, ymax = totleaves), fill = "black") +
+  geom_ribbon(aes(doy, ymin = 0, ymax = green), fill = "green") +
+  geom_ribbon(aes(doy, ymin = 0, ymax = dead), fill = "brown") +
+  facet_wrap(~year + rot_trt, scales = "free_x", ncol = 2)
+
+
+d_simp %>% 
+  group_by(year, doy, rot_trt) %>% 
+  summarise_if(is.numeric, mean, na.rm = T) %>% 
+  ggplot() +
+  geom_ribbon(aes(doy, ymin = 0, ymax = totleaves), fill = "brown") +
+  geom_ribbon(aes(doy, ymin = 0, ymax = green), fill = "green4") +
+  facet_wrap(~year + rot_trt, scales = "free_x", ncol = 2)
