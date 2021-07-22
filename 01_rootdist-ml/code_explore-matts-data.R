40af44acce17b1f182b741ca1649c29cc50f5021
vanichols <virginia.nichols@gmail.com>
2021-07-22 07:14:39 -0500

    calc roots added

--- 01_rootdist-ml/code_explore-matts-data.R
+++ 01_rootdist-ml/code_explore-matts-data.R
@@ -20,25 +20,32 @@ rm <-
   group_by(year) %>% 
   mutate(min_dap = min(dap),
          max_dap = max(dap),
-         minmax_x = ifelse(dap == min_dap, "min", 
-                           ifelse(dap == max_dap, "max", NA))) %>% 
+         minmax_x = ifelse(dap == min_dap, "beg", 
+                           ifelse(dap == max_dap, "end", NA))) %>% 
   filter(!is.na(minmax_x)) %>% 
   left_join(mrs_plotkey)
 
-rm %>% 
+rm_add <- 
+  rm %>% 
   select(year, block, rot_trt, minmax_x, roots_kgha) %>% 
-  pivot_wider(names_from = minmax_x, values_from = roots_kgha) #%>% 
-  clean_names() %>% 
-  group_by(year, block, rot_trt) %>% 
-  summarise(max = sum(max, na.rm = T),
-            min = sum(min, na.rm = T)) %>% 
-  group_by(year, rot_trt) %>% 
-  summarise(max = mean(max),
-            min = mean(min)) %>% 
-  mutate(roots_added_g = max - min)
+  pivot_wider(names_from = minmax_x, values_from = roots_kgha) %>% 
+  mutate(roots_added = end - beg)
 
+rm_add %>% write_csv("01_rootdist-ml/dat_roots-added.csv")
 
+#--lot of error
+rm_add %>% 
+  ggplot(aes(rot_trt, roots_added)) + 
+  stat_summary(geom = "bar") + 
+  stat_summary() + 
+  facet_grid(.~year)
+  
+#--in 2020 the 4 yr had less root stuff at the end of the season
 rm %>% 
-  ggplot(aes(rot_trt, roots_added_g)) + 
-  geom_point() + 
-  facet_grid(year ~ .)
+  select(year, block, rot_trt, minmax_x, roots_kgha) %>%
+  group_by(year, rot_trt, minmax_x) %>% 
+  summarise(roots_kgha = mean(roots_kgha, na.rm = T)) %>%
+  ggplot(aes(rot_trt, roots_kgha, fill = minmax_x)) + 
+  geom_col(position = position_dodge()) + 
+  facet_grid(.~year)
+
